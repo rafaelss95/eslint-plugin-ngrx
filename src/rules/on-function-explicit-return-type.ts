e8bee57e18197e224f3cbd94fbd83c1302182702
Rafael Santana <rafaelsantana3095@gmail.com>
2021-02-07 14:25:12 -0300

    feat: add suggestion for on-function-explicit-return-type rule

--- src/rules/on-function-explicit-return-type.ts
+++ src/rules/on-function-explicit-return-type.ts
@@ -1,11 +1,15 @@
-import { ESLintUtils, TSESTree } from '@typescript-eslint/experimental-utils'
-
+import { ASTUtils, ESLintUtils } from '@typescript-eslint/experimental-utils'
+import type { TSESLint, TSESTree } from '@typescript-eslint/experimental-utils'
 import { onFunctionWithoutType, docsUrl } from '../utils'
 
 export const ruleName = 'on-function-explicit-return-type'
 
-export const messageId = 'onFunctionExplicitReturnType'
-export type MessageIds = typeof messageId
+export const onFunctionExplicitReturnType = 'onFunctionExplicitReturnType'
+export const onFunctionExplicitReturnTypeSuggest =
+  'onFunctionExplicitReturnTypeSuggest'
+export type MessageIds =
+  | typeof onFunctionExplicitReturnType
+  | typeof onFunctionExplicitReturnTypeSuggest
 
 type Options = []
 
@@ -18,21 +22,55 @@ export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
       description: 'On function should have an explicit return type',
       recommended: 'error',
     },
+    fixable: 'code',
     schema: [],
     messages: {
-      [messageId]:
-        'On functions should have an explicit return type when using arrow functions, on(action, (state):State => {}',
+      [onFunctionExplicitReturnType]:
+        'On functions should have an explicit return type when using arrow functions, `on(action, (state): State => {}`.',
+      [onFunctionExplicitReturnTypeSuggest]: 'Add explicit return type.',
     },
   },
   defaultOptions: [],
   create: (context) => {
+    const sourceCode = context.getSourceCode()
+
     return {
-      [onFunctionWithoutType](node: TSESTree.TSTypeReference) {
+      [onFunctionWithoutType](node: TSESTree.ArrowFunctionExpression) {
         context.report({
+          suggest: [
+            {
+              fix: (fixer) => getFixes(node, sourceCode, fixer),
+              messageId: onFunctionExplicitReturnTypeSuggest,
+            },
+          ],
           node,
-          messageId,
+          messageId: onFunctionExplicitReturnType,
         })
       },
     }
   },
 })
+
+function getFixes(
+  node: TSESTree.ArrowFunctionExpression,
+  sourceCode: Readonly<TSESLint.SourceCode>,
+  fixer: TSESLint.RuleFixer,
+): TSESLint.RuleFix | TSESLint.RuleFix[] {
+  const { params } = node
+  const [firstParam] = params
+  const [lastParam] = params.slice(-1)
+  const previousToken = sourceCode.getTokenBefore(firstParam)
+  const isParenthesized =
+    previousToken && ASTUtils.isOpeningParenToken(previousToken)
+
+  if (isParenthesized) {
+    const nextToken = sourceCode.getTokenAfter(lastParam)
+
+    return fixer.insertTextAfter(nextToken ?? lastParam, ': State')
+  }
+
+  return [
+    fixer.insertTextBefore(firstParam, '('),
+    fixer.insertTextAfter(lastParam, '): State'),
+  ]
+}
