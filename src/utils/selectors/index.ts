eb1ce89f6c18b7f450f54bf8dad4cbfe8e95bc5c
Rafael Santana <rafaelsantana3095@gmail.com>
2021-02-07 22:29:47 -0300

    refactor: change selectors to grab stores properly

--- src/utils/selectors/index.ts
+++ src/utils/selectors/index.ts
@@ -9,10 +9,10 @@ export const actionCreatorPropsComputed = `${actionCreatorProps} > TSTypeParamet
 
 export const constructorExit = `MethodDefinition[kind='constructor']:exit`
 
-export const dispatchInEffects = `ClassProperty > CallExpression:has(Identifier[name="createEffect"]) CallExpression > MemberExpression:has(Identifier[name="dispatch"]):has(MemberExpression > Identifier[name="store"])`
+export const dispatchInEffects = `ClassProperty > CallExpression:has(Identifier[name='createEffect']) CallExpression > MemberExpression:has(Identifier[name='dispatch']):has(MemberExpression > Identifier[name='store'])`
 
-export const injectedStore = `MethodDefinition[kind='constructor'] Identifier[typeAnnotation.typeAnnotation.typeName.name="Store"]`
-export const typedStore = `MethodDefinition[kind='constructor'] Identifier>TSTypeAnnotation>TSTypeReference[typeName.name="Store"][typeParameters.params]`
+export const injectedStore = `MethodDefinition[kind='constructor'] Identifier[typeAnnotation.typeAnnotation.typeName.name='Store']`
+export const typedStore = `MethodDefinition[kind='constructor'] Identifier>TSTypeAnnotation>TSTypeReference[typeName.name='Store'][typeParameters.params]`
 
 export const ngModuleDecorator = `ClassDeclaration > Decorator > CallExpression[callee.name='NgModule']`
 
@@ -20,10 +20,9 @@ export const ngModuleProviders = `${ngModuleDecorator} ObjectExpression Property
 
 export const ngModuleImports = `${ngModuleDecorator} ObjectExpression Property[key.name='imports'] > ArrayExpression CallExpression[callee.object.name='EffectsModule'][callee.property.name=/forRoot|forFeature/] ArrayExpression > Identifier`
 
-export const pipeableSelect = `CallExpression[callee.property.name="pipe"] CallExpression[callee.name="select"]`
-export const storeSelect = `CallExpression[callee.object.property.name='store'][callee.property.name='select']`
-
-export const select = `${pipeableSelect} Literal, ${storeSelect} Literal, ${pipeableSelect} ArrowFunctionExpression, ${storeSelect} ArrowFunctionExpression`
+export const pipeExpression = `callee.property.name='pipe'`
+export const selectOperator = `CallExpression[callee.name='select']`
+export const pipeableSelect = `CallExpression[${pipeExpression}] ${selectOperator}`
 
 export const onFunctionWithoutType = `CallExpression[callee.name='createReducer'] CallExpression[callee.name='on'] > ArrowFunctionExpression:not([returnType.typeAnnotation],:has(CallExpression))`
 
